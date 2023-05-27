fbc7c4c4918d6f2fefda03da2508fbf69412684c
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-05-27 12:16:24 +0300

    parser and task1 done

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
-import _ from 'lodash'
+import _ from "lodash";
 
-export default function solution(content){
+export default function solution(content) {
   // BEGIN
 
-  // END
+  const rows = content
+    .trim()
+    .split("\n")
+    .map((row) => row.split(","));
+  const headerNames = rows[0];
+  const [
+    date,
+    maxT,
+    minT,
+    humidity,
+    pressure,
+    windSpeed,
+    windDir,
+    city,
+    state,
+    timeZone,
+  ] = headerNames;
+  const data = rows.slice(1).map((row) => {
+    const structuredRow = new Map();
+    row.forEach((element, i) => {
+      let value = element;
+      if ([maxT, minT, humidity, pressure, windSpeed].includes(headerNames[i]))
+        value = Number(element);
+      structuredRow.set(headerNames[i], value);
+    });
+    return structuredRow;
+  });
+  // #1 ---
+  console.log(`Count: ${data.length}`);
 
+  // END
 }
